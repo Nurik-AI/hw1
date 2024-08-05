54b7167c6d9a2717db8a3955054ed2c404608479
Nurik-AI <zulpuevnursultan1@gmail.com>
2024-08-05 18:19:54 +0600

    Add files via upload

---
+++ доп дз1 2-м/main.js
@@ -0,0 +1,13 @@
+ 
+let con = confirm(' вам есть 18?')
+
+if (con > false) {
+    let prom = prompt ('введите имя')
+    if (prom) {
+        alert( 'добро пожаловать!' + prom)
+    } else 
+        ('вход запрещен!')
+    
+        }else {
+        alert('вход запрещен!')
+    }
